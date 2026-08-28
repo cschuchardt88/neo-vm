283b25b06b803f5377f428d695fa37a2e45c6434
Christopher Schuchardt <8141309+cschuchardt88@users.noreply.github.com>
2026-08-27 23:13:03 -0400

    Cover VmFeatures off-paths in unit tests
    Exercise SHR/SUBSTR/HASKEY/PICKITEM/REMOVE/SETITEM/CONVERT with
    reduced Features so patch coverage includes the pre-fork branches.

--- tests/Neo.VM.Tests/UT_VmFeatures.cs
+++ tests/Neo.VM.Tests/UT_VmFeatures.cs
@@ -91,6 +91,156 @@ public class UT_VmFeatures
     }
 
     [TestMethod]
+    public void Shr_ZeroShift_WithoutBoundedShift_DoesNotConsumeValue()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        sb.EmitPush(1);
+        sb.Emit(OpCode.NEWBUFFER);
+        sb.EmitPush(0);
+        sb.Emit(OpCode.SHR);
+        Assert.AreEqual(VMState.HALT, Run(engine, sb));
+        Assert.IsInstanceOfType(engine.ResultStack.Pop(), typeof(Buffer));
+    }
+
+    [TestMethod]
+    public void Shr_ZeroShift_WithBoundedShift_ConsumesValue()
+    {
+        using var engine = Engine(VmFeatures.BoundedShift);
+        using var sb = new ScriptBuilder();
+        sb.EmitPush(1);
+        sb.Emit(OpCode.NEWBUFFER);
+        sb.EmitPush(0);
+        sb.Emit(OpCode.SHR);
+        Assert.AreEqual(VMState.FAULT, Run(engine, sb));
+    }
+
+    [TestMethod]
+    public void SubStr_WithoutSafeSubStr_AllowsUncheckedAdd()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        byte[] data = [1, 2, 3, 4];
+        sb.EmitPush(data);
+        sb.EmitPush(1);
+        sb.EmitPush(2);
+        sb.Emit(OpCode.SUBSTR);
+        Assert.AreEqual(VMState.HALT, Run(engine, sb));
+        byte[] expected = [2, 3];
+        CollectionAssert.AreEqual(expected, engine.ResultStack.Pop().GetSpan().ToArray());
+    }
+
+    [TestMethod]
+    public void SubStr_Wrap_WithSafeSubStr_Faults()
+    {
+        using var engine = Engine(VmFeatures.SafeSubStr);
+        using var sb = new ScriptBuilder();
+        byte[] data = [1, 2];
+        sb.EmitPush(data);
+        sb.EmitPush(int.MaxValue);
+        sb.EmitPush(1);
+        sb.Emit(OpCode.SUBSTR);
+        Assert.AreEqual(VMState.FAULT, Run(engine, sb));
+    }
+
+    [TestMethod]
+    public void SubStr_Wrap_WithoutSafeSubStr_DoesNotUseCheckedAdd()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        byte[] data = [1, 2];
+        sb.EmitPush(data);
+        sb.EmitPush(int.MaxValue);
+        sb.EmitPush(1);
+        sb.Emit(OpCode.SUBSTR);
+        Assert.AreEqual(VMState.FAULT, Run(engine, sb));
+    }
+
+    [TestMethod]
+    public void HasKey_BufferLargeIndex_WithoutStrict_ReturnsFalse()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        sb.EmitPush(1);
+        sb.Emit(OpCode.NEWBUFFER);
+        sb.EmitPush((BigInteger)engine.Limits.MaxItemSize);
+        sb.Emit(OpCode.HASKEY);
+        Assert.AreEqual(VMState.HALT, Run(engine, sb));
+        Assert.IsFalse(engine.ResultStack.Pop().GetBoolean());
+    }
+
+    [TestMethod]
+    public void HasKey_BufferAndByteString()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        sb.EmitPush(1);
+        sb.Emit(OpCode.NEWBUFFER);
+        sb.EmitPush(0);
+        sb.Emit(OpCode.HASKEY);
+        byte[] data = [9];
+        sb.EmitPush(data);
+        sb.EmitPush(0);
+        sb.Emit(OpCode.HASKEY);
+        Assert.AreEqual(VMState.HALT, Run(engine, sb));
+        Assert.IsTrue(engine.ResultStack.Pop().GetBoolean());
+        Assert.IsTrue(engine.ResultStack.Pop().GetBoolean());
+    }
+
+    [TestMethod]
+    public void PickItem_NegativeIndex_Faults()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        sb.Emit(OpCode.NEWARRAY0);
+        sb.EmitPush(BigInteger.MinusOne);
+        sb.Emit(OpCode.PICKITEM);
+        Assert.AreEqual(VMState.FAULT, Run(engine, sb));
+    }
+
+    [TestMethod]
+    public void Remove_NegativeIndex_Faults()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        sb.EmitPush(1);
+        sb.EmitPush(1);
+        sb.Emit(OpCode.PACK);
+        sb.EmitPush(BigInteger.MinusOne);
+        sb.Emit(OpCode.REMOVE);
+        Assert.AreEqual(VMState.FAULT, Run(engine, sb));
+    }
+
+    [TestMethod]
+    public void SetItem_NegativeIndex_Faults()
+    {
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        sb.EmitPush(1);
+        sb.EmitPush(1);
+        sb.Emit(OpCode.PACK);
+        sb.EmitPush(BigInteger.MinusOne);
+        sb.EmitPush(0);
+        sb.Emit(OpCode.SETITEM);
+        Assert.AreEqual(VMState.FAULT, Run(engine, sb));
+    }
+
+    [TestMethod]
+    public void ConvertTo_WithoutLimits_UsesDefaultFeatures()
+    {
+        StackItem item = true;
+        var converted = item.ConvertTo(StackItemType.Boolean);
+        Assert.IsTrue(converted.GetBoolean());
+
+        using var engine = Engine(VmFeatures.None);
+        using var sb = new ScriptBuilder();
+        sb.EmitPush(true);
+        sb.Emit(OpCode.CONVERT, [(byte)StackItemType.Boolean]);
+        Assert.AreEqual(VMState.HALT, Run(engine, sb));
+        Assert.IsTrue(engine.ResultStack.Pop().GetBoolean());
+    }
+
+    [TestMethod]
     public void DefaultLimits_EnableCurrentFeatures()
     {
         Assert.IsTrue(ExecutionEngineLimits.Default.Has(VmFeatureSets.Current));
